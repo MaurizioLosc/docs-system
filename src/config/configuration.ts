4b6969494197a39067e48b12af7277e15a65f59f
MaurizioLosc <ddhlytked5@gmx.us>
2026-03-01 19:35:36 +0530

    Update

---
+++ src/config/configuration.ts
@@ -0,0 +1,7 @@
+export default () => ({
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+    PROJECT_NAME: process.env.PROJECT_NAME,
+})
